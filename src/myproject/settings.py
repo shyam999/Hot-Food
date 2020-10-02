124d3fde06cca4a9b1e41b1f8b39ff0446cf2d3d
iamlovelesh <rghvn29@gmail.com>
2020-10-02 20:02:52 +0530

    Added Dynamic Generation of Invoice

--- src/myproject/settings.py
+++ src/myproject/settings.py
@@ -107,8 +107,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
+
+
 
 # media settings
 MEDIA_URL = '/media/'
